ca12294ca62f0ccf2a1eeaa3add3a769b5959f29
pablo galvan <gvpablodaniel@gmail.com>
2024-02-08 09:01:21 -0300

    create CharacterViewModel

---
+++ app/src/main/java/com/ontop/test/rickandmorty/presentation/CharacterViewModel.kt
@@ -0,0 +1,35 @@
+package com.ontop.test.rickandmorty.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.ontop.test.rickandmorty.domain.model.Character
+import com.ontop.test.rickandmorty.domain.repository.IRickAndMortyRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class CharacterViewModel @Inject constructor(
+    private val repository: IRickAndMortyRepository,
+) : ViewModel() {
+
+    private val _name = MutableStateFlow("")
+    val name = _name.asStateFlow()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val characters: Flow<PagingData<Character>> = _name.flatMapLatest { query ->
+        repository.getCharacters(query)
+    }.cachedIn(viewModelScope)
+
+    fun setQuery(name: String) {
+        _name.update { name }
+    }
+
+}
